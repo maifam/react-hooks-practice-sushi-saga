3454085c24daeaf15a89566f3fb5cf11d92e1025
Mai Pham <maitphm@gmail.com>
2021-02-05 17:12:16 -0500

    core deliverables done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [sushi, setSushi] = useState([])
+  const [limit, setLimit] = useState(0)
+  const [plates, setPlates] = useState([])
+  const [wallet, setWallet] = useState(100)
+
+  useEffect(() => {
+    fetch(API)
+    .then(res => res.json())
+    .then(sushiArr => {
+      setSushi(sushiArr)
+    })
+  }, [] )
+
+  const sushiDisplayed = sushi.slice(limit, limit + 4)
+
+  function moreSushi(){
+    setLimit(limit+4)
+  }
+
+
+  function handleSushi(newPlate, sushiPrice) {
+    if (sushiPrice < wallet) {
+      setWallet(wallet - sushiPrice)
+      setPlates([...plates, newPlate])
+    } else {
+      alert ("You don't have enough for this sushi")
+    }
+  }
+
+  
+ 
+
+  console.log(sushi)
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushi={sushiDisplayed} moreButton={moreSushi} onHandleSushi={handleSushi}/>
+      <Table plates={plates} wallet={wallet}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import Sushi from './Sushi'
 import MoreButton from "./MoreButton";
 
-function SushiContainer(props) {
+function SushiContainer({sushi, moreButton, onHandleSushi}) {
+
+  const [sushiEaten, setSushiEaten] = useState(false)
+
+ function removeSushi(sushiToRemove, sushiPrice) {
+   onHandleSushi(sushiToRemove, sushiPrice)
+ }
+
+  const sushiObj = sushi.map((sush) => {
+    return <Sushi key={sush.id} 
+                  name={sush.name} 
+                  img_url={sush.img_url} 
+                  price={sush.price}
+                  removeSushi={removeSushi}
+            />
+  })
+
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiObj}
+      <MoreButton moreButton={moreButton}/>
     </div>
   );
 }
